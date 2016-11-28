c9e50e9d57080df82fdfda5a7b00bc7e9ebb5a38
wb-baopan <wb-baopan@le.com>
2016-11-28 09:37:14 +0800

    create new web project demo or temple
    一个web+spring+maven的项目，或者说是一个模板

---
+++ src/main/java/com/bp/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.bp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * @author current_bp
+ * @createTime 20161128
+ *
+ */
+
+@Controller("helloController")
+@RequestMapping("/hello")
+public class HelloController {
+
+	
+	@RequestMapping(value = "sayHello", method = RequestMethod.GET)
+	@ResponseBody
+	public String sayHello(){
+		return "baopan";
+	}
+}
